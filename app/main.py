928061822724e79477dbb526d8ddc72a3c6566a3
Talita <talita.anthonio@yahoo.com>
2025-02-15 21:40:26 +0100

    add cors to communicate with frontend

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi.responses import RedirectResponse
 from fastapi import FastAPI
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def docs_redirect():
